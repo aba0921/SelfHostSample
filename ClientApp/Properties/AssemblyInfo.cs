33e7df31215af0aab58358428d6c757f0afcf410
seki <seki@arara.com>
2018-03-26 22:30:59 +0900

    Implement ASP.NET WebAPI SelfHost with .NET 4.0

--- ClientApp/Properties/AssemblyInfo.cs
+++ ClientApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SelfHost")]
+[assembly: AssemblyTitle("ClientApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SelfHost")]
+[assembly: AssemblyProduct("ClientApp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a6674b44-a41f-462d-aad3-59d2024084da")]
+[assembly: Guid("5a4339c2-2cdd-475f-baa8-45236bd04304")]
 
 // Version information for an assembly consists of the following four values:
 //
